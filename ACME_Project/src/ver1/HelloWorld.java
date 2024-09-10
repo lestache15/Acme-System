2beacb5a0d12e1a931fdab1b9db61673c41f9394
Leslie Dawn <leslieschoolnotes@gmail.com>
2024-09-09 21:45:06 -0400

    Leslie's commit

--- ACME_Project/src/ver1/HelloWorld.java
+++ ACME_Project/src/ver1/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello from Eclipse");
+		System.out.println("Leslie is working locally on her branch");
 	}
 
 }
